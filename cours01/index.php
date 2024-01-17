bc701e99393a63939a86e25af9b44f2414f8b6de
AugustinPech <80385451+AugustinPech@users.noreply.github.com>
2024-01-17 14:29:35 +0100

    page 404.html working

--- cours01/index.php
+++ cours01/index.php
@@ -1,11 +1,19 @@
 
 <?php
-$page = $_GET["page"];
-if ($page =='Contact') {
-        include 'pages/contact.php';
-    } else if ($page =='Hobby') {
-        include 'pages/hobby.php';
-    } else {
-        include 'pages/Augustin.php';
-    }
+
+$page = filter_input(INPUT_GET, 'page', FILTER_SANITIZE_SPECIAL_CHARS);
+echo $page;
+if ($page == 'Contact') {
+    include 'pages/contact.php';
+} else if ($page == 'Hobby') {
+    include 'pages/hobby.php';
+} else if ($page == 'CV') {
+    include 'pages/Augustin.php';
+} else {
+    header('HTTP/1.0 404 Not Found');
+    include 'pages/header.php';
+    include 'pages/404.html';
+    include 'pages/footer.php';
+}
+
 ?>
