21620e05cbbda3afacabb78f66446032781deda6
AugustinPech <80385451+AugustinPech@users.noreply.github.com>
2024-01-17 15:26:25 +0100

    etape 3 finie

--- cours01/index.php
+++ cours01/index.php
@@ -2,7 +2,7 @@
 <?php
 
 $page = filter_input(INPUT_GET, 'page', FILTER_SANITIZE_SPECIAL_CHARS);
-echo $page;
+
 if ($page == 'Contact') {
     include 'pages/contact.php';
 } else if ($page == 'Hobby') {
